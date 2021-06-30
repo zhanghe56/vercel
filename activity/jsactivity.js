58e221f35e3ca936704fc6ebe21bfd42b916f317
ZH <18744355478@163.com>
2021-06-30 19:51:08 +0800

    同步仓库

---
+++ activity/jsactivity.js
@@ -0,0 +1,11 @@
+function gameder(){
+alert("7月4日左右上线，敬请期待~")
+}
+
+function gamemg(){
+alert("目前还在开发阶段，暂时没有上线计划，敬请期待~")
+}
+
+function gamefashion(){
+window.open("https://activity.vercel.app/laofashion/index.html")
+}
---
+++ banner/gaokaojs.js
@@ -0,0 +1,31 @@
+function jyksy1(){
+window.XB.openwithbrowser('http://www.jleea.edu.cn/');
+}
+
+function jyksy2(){
+window.XB.openwithbrowser('http://www.jleea.com.cn/');
+}
+
+function jydst(){
+window.XB.openwithbrowser('http://www.jletv.cn/');
+}
+
+function yfydcx(){
+alert("敬请期待！");
+}
+
+function yxkyggk(){
+window.XB.openwithbrowser('https://gaokao.chsi.com.cn/wap/sch/schlist');
+}
+
+function zykyggk(){
+window.XB.openwithbrowser('https://gaokao.chsi.com.cn/zyk/zybk/wap/');
+}
+
+function zsznxc(){
+window.XB.openwithbrowser('http://www.jleea.com.cn/2021/0621/3776.html');
+}
+
+function gkzyjleea(){
+window.XB.openwithbrowser('https://gkzy.jleea.com.cn/');
+}
