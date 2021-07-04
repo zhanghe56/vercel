1fee4f3e73fa1e1efe632553051f514291153eec
zhanghe56 <63276160+zhanghe56@users.noreply.github.com>
2021-07-04 16:27:15 +0800

    Update dergame.js
    积分规则调整

--- activity/der/dergame.js
+++ activity/der/dergame.js
@@ -9,9 +9,9 @@ function checkder(){
          if (checkpoint>999)
          {
          	
-         if (checkpoint>9999)
+         if (checkpoint>2999)
          {
-         	if (checkpoint>99999)
+         	if (checkpoint>5999)
          {
 //100000以上
 document.getElementById("derimg").src = "people5.png";
@@ -40,7 +40,7 @@ document.getElementById("derlevel").innerHTML = "der哥";
 document.getElementById("derlevel").innerHTML = "一点也不der";
 }       
         } else {
-            alert("这里有充实的游戏，这里有精湛的美工，这里有一切你想要的！欢迎游玩由XB小报实属灵魂画师团队出品的新游戏——史上最der的游戏！您要做的只有点击那个漂亮的小人。开始游戏吧~PS：特别感谢你寒王提供素材。der等级共5段，0~99，100~1k，1k~10k，10k~100k以及100k以上");
+            alert("这里有充实的游戏，这里有精湛的美工，这里有一切你想要的！欢迎游玩由XB小报实属灵魂画师团队出品的新游戏——史上最der的游戏！您要做的只有点击那个漂亮的小人。开始游戏吧~PS：特别感谢你寒王提供素材。der等级共5段，0~99，100~1k，1k~3k，3k~6k以及6k以上");
            localStorage.der = "0"
            document.getElementById("derresult").innerHTML = localStorage.der;
            document.getElementById("derlevel").innerHTML = "一点也不der";
@@ -58,5 +58,5 @@ document.getElementById("derresult").innerHTML = localStorage.der;
 }
 
 function introduction(){
-alert("这里有充实的游戏，这里有精湛的美工，这里有一切你想要的！欢迎游玩由XB小报实属灵魂画师团队出品的新游戏——史上最der的游戏！您要做的只有点击那个漂亮的小人。开始游戏吧~PS：特别感谢你寒王提供素材。der等级共5段，0~99，100~1k，1k~10k，10k~100k以及100k以上");
+alert("这里有充实的游戏，这里有精湛的美工，这里有一切你想要的！欢迎游玩由XB小报实属灵魂画师团队出品的新游戏——史上最der的游戏！您要做的只有点击那个漂亮的小人。开始游戏吧~PS：特别感谢你寒王提供素材。der等级共5段，0~99，100~1k，1k~3k，3k~6k以及6k以上");
 }
