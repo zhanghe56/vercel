77e4937ffbad3025326aa57452983f6030020f06
zhanghe56 <18744355478@163.com>
2021-08-10 18:31:02 +0800

    更新至2.3.0810rev1.69版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,5 +1,5 @@
 function jumptonew() {
-window.location.href="https://wwa.lanzoui.com/imSs1sailna"}
+window.location.href="https://wwa.lanzoui.com/itCbzsggcsf"}
 function checkvision() {
 sessionStorage.newvision=("99999");
 sessionStorage.updateday=("2021.08.06");
