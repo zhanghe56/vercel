122cf115194fdd626bc2ed42e068fb49a2cb877d
zhanghe56 <18744355478@163.com>
2021-09-17 17:29:30 +0800

    更新至2.3.0917rev1.74版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,5 +1,5 @@
 function jumptonew() {
-window.location.href="https://www.lanzouw.com/iztWRu4a9pi"}
+window.location.href="https://www.lanzouw.com/if2JBu59szc"}
 function checkvision() {
 sessionStorage.newvision=("99999");
 sessionStorage.updateday=("2021.08.06");
