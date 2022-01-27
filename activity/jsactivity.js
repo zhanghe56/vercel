288d238d99bcb2482a3e43ce67901aa9d9c78c16
zhanghe56 <18744355478@163.com>
2022-01-27 20:58:54 +0800

    修改了合成老fashion的页面链接

--- activity/jsactivity.js
+++ activity/jsactivity.js
@@ -7,5 +7,5 @@ alert("目前还在开发阶段，暂定7月末上线，敬请期待~")
 }
 
 function gamefashion(){
-window.open("https://zhanghe56.github.io/vercel/laofashion/index.html")
+window.open("https://zhanghe56.gitee.io/android_main/activity/fashion2nd/banner/guide/guide.html")
 }
