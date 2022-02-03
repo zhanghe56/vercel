942240f66e8ff4bc727cadb3af99b90ff2d1bd4e
H Z <18744355478@163.com>
2022-02-03 10:10:12 +0800

    更新至2.4.0203rev1.82版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,5 +1,5 @@
 function jumptonew() {
-window.location.href="https://wwi.lanzouw.com/iukBszbmtkh"}
+window.location.href="https://wwi.lanzouw.com/ianS0zj5mub"}
 function checkvision() {
 sessionStorage.newvision=("99999");
 sessionStorage.updateday=("2021.08.06");
