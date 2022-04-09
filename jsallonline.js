db970a603438ee3c5edf47482ce7c37f82cc5bbf
ZH <18744355478@163.com>
2022-04-09 22:42:49 +0800

    更新至2.4.0409rev1.90版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,5 +1,5 @@
 function jumptonew() {
-window.location.href="https://wwm.lanzouw.com/iKIiZ02ezt0b"}
+window.location.href="https://wwm.lanzouw.com/inzVh02x70mf"}
 function checkvision() {
 sessionStorage.newvision=("99999");
 sessionStorage.updateday=("2021.08.06");
