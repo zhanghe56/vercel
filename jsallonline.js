13333cf0ce62e31894a3e2734dca7116c46f44af
zhanghe56 <18744355478@163.com>
2023-01-20 21:47:53 +0800

    更新至2.5.0121rev1.97版本

--- jsallonline.js
+++ jsallonline.js
@@ -1,5 +1,5 @@
 function jumptonew() {
-window.location.href="https://wwm.lanzouw.com/iOE5F0e0a2xa"}
+window.location.href="https://wwzx.lanzouw.com/iI9Z40li99zi"}
 function jumptobanner() {
 window.location.replace("activity/ver2.5/banner.html")}
 function checkvision() {
